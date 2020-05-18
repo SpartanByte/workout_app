53943c3133620f26a0eda816ac4f5d2d26c3f9c2
Brian Wardwell <brian.wardwell@codelation.com>
2020-05-18 07:31:48 -0500

    added exercise scope and example of exercise older than 7 days

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -12,6 +12,9 @@ RSpec.feature "Listing Exercises" do
     @e2 = @john.exercises.create(duration_in_min: 55, workout: "Weight lifting",
         workout_date: 2.days.ago)
 
+    @e3 = @john.exercises.create(duration_in_min: 35, workout: "Cardio",
+        workout_date: 8.days.ago)
+
     scenario "show users workout for last 7 days" do
         visit '/'
 
@@ -24,6 +27,10 @@ RSpec.feature "Listing Exercises" do
         expect(page).to have_content(@e2.duration_in_min)
         expect(page).to have_content(@e2.workout)
         expect(page).to have_content(@e2.workout_date)
+
+        expect(page).not_to have_content(@e3.duration_in_min)
+        expect(page).not_to have_content(@e3.workout)
+        expect(page).not_to have_content(@e3.workout_date)
     end
 
     scenario "shows no exercises if none exist" do
